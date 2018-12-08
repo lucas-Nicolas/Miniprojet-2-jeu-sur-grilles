a1c61d74d1f56dad78a1e2910abc49eb6b12d8f3
Lucas Nicolas <Lucasnicolasnkok@gmail.com>
2018-12-08 12:51:15 +0100

    Pressurelate & Conception début

---
+++ CONCEPTION.md
@@ -0,0 +1,8 @@
+#Conception 
+
+#étape 4
+
+Lors de l'étape 4, nous avons choisi de faire des classes abstraites Collectable et Switch représentant respectivement les objets à ramasser tels que les pommes ou les clefs et les boutons ayant vocation à s'activer et se désactiver. 
+Ces deux classes héritent de la classe AreaEntity puisque nous considèrons qu'elles nécessitent aussi bien l'une que l'autre une aire d'appartenance ainsi q'une position au sein de cette aire. 
+
+Toutefois nous  
---
+++ java/ch/epfl/cs107/play/game/enigme/actor/PressurePlate.java
@@ -0,0 +1,58 @@
+package ch.epfl.cs107.play.game.enigme.actor;
+
+import ch.epfl.cs107.play.game.areagame.Area;
+import ch.epfl.cs107.play.game.areagame.actor.Sprite;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.window.Canvas;
+
+public class PressurePlate extends Switch {
+    private final Sprite groundLightOn = new Sprite("GroundLightOn", 1, 1f,this);
+    private final Sprite groundPlateOff = new Sprite("GroundPlateOff", 1, 1f,this);
+    private final float deactivationTime;
+
+    public PressurePlate(Area area, DiscreteCoordinates position, float deactivationTime) {
+        super(area, position, false);
+        this.deactivationTime = deactivationTime;
+    }
+
+    public PressurePlate(Area area, DiscreteCoordinates position, boolean isActivated) {
+        super(area, position, isActivated);
+        this.deactivationTime = 0.3f;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        if(isActivated()){
+            groundLightOn.draw(canvas);
+        }
+        else{
+            groundPlateOff.draw(canvas);
+        }
+    }
+
+    @Override
+    public boolean takeCellSpace() {
+        return false;
+    }
+
+    @Override
+    public boolean isViewInteractable() {
+        return false;
+    }
+
+    @Override
+    public boolean isCellInteractable() {
+        return true;
+    }
+
+    @Override
+    protected void setIsActivated() {
+        try {
+            Thread.sleep((long)(deactivationTime*1000));
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+        }
+        super.setIsActivated();
+    }
+}
