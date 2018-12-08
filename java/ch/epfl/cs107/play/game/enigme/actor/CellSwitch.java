afc0006d2fe2b2b3d8fac4dfc583d015d6b6b0bc
Lucas Nicolas <Lucasnicolasnkok@gmail.com>
2018-12-08 15:08:04 +0100

    6.5

--- java/ch/epfl/cs107/play/game/enigme/actor/CellSwitch.java
+++ java/ch/epfl/cs107/play/game/enigme/actor/CellSwitch.java
@@ -1,9 +1,13 @@
 package ch.epfl.cs107.play.game.enigme.actor;
 
 import ch.epfl.cs107.play.game.areagame.Area;
+import ch.epfl.cs107.play.game.areagame.handler.AreaInteractionVisitor;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
 
 public abstract class CellSwitch extends Switch {
+    private boolean switchedOnPreviousFrame;
+    private boolean canBeSwitched;
+
 
     public CellSwitch(Area area, DiscreteCoordinates position) {
         super(area, position, false);
@@ -24,4 +28,26 @@ public abstract class CellSwitch extends Switch {
     public boolean isCellInteractable() {
         return true;
     }
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        if(switchedOnPreviousFrame){
+            canBeSwitched = false;
+
+        }else {
+            canBeSwitched = true;
+        }
+        switchedOnPreviousFrame = false;
+
+    }
+
+    @Override
+    public void acceptInteraction(AreaInteractionVisitor v) {
+        if(canBeSwitched) {
+            super.acceptInteraction(v);
+        }
+        switchedOnPreviousFrame = true;
+
+    }
+
 }
